c435045b1c958548bdf0b4ac382ec4fb4a1541ce
ErfanMojibi <mojibierfan@gmail.com>
2023-03-07 22:46:49 +0330

    small edit in login and signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,9 +5,9 @@ export default function LoginPage() {
     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <Header
-          heading="LoginForm to your account"
+          heading="Login to your account"
           paragraph="Don't have an account yet? "
-          linkName="SignupForm"
+          linkName="Signup"
           linkUrl="/signup"
         />
         <LoginForm />
